f197e3a33aa87094a19029ee903c544d76fcc284
Jonas Ribeiro <jonas.ribeiro01@fatec.sp.gov.br>
2024-04-14 16:24:57 -0300

    Vários arquivos - configurando o workflow

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Response as ExResponse, Request as ExRequest } from "express";
+import swaggerUi from "swagger-ui-express";
 import cors from 'cors';
 import { AppDataSource } from './database/data-source';
 import routers from './app/routes/routes';
@@ -9,6 +10,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routers);
+
+// Using the router to publish Swagger + TSOA documentation
+app.use("/docs", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
+  return res.send(
+    swaggerUi.generateHTML(await import("./swagger/swagger.json"))
+  );
+});
 
 AppDataSource.initialize().then(async () => {
     console.log('Database Ok');
