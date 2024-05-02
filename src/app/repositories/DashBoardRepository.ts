568b3a850395767c9217cd4679a127fc77d06e7e
Miguel Carvalho <doxito007@gmail.com>
2024-05-02 11:15:37 -0300

    Teste

--- src/app/repositories/DashBoardRepository.ts
+++ src/app/repositories/DashBoardRepository.ts
@@ -1,21 +1,26 @@
 import { AppDataSource } from "../../database/data-source";
 import Measure from "../entities/Measure";
-import measure from "../entities/Measure";
 import StationParameter from "../entities/StationParameter";
 import IMeasure from "../interfaces/IMeasure";
 import IStationParameter from "../interfaces/IStationParameter";
-import stationParameterRepository from "./StationParameterRepository";
 
 
 const stationParameter = AppDataSource.getRepository(StationParameter);
 const dashBoardRepository = AppDataSource.getRepository(Measure);
 
 const getDahsBoardData = async (id: number): Promise<IStationParameter[]> => {
-    const dashBoardList = await stationParameter.find({
-        relations: ["station", "parameterType", "measure"], // Inclua aqui as relações necessárias
-        where: { station: { id_station: id } }
-    });
+    // const dashBoardList = await stationParameter.find({
+    //     relations: ["station", "parameter_type", "measures"],
+    // });
     
+    const dashBoardList = await stationParameter
+    .createQueryBuilder("stationParameter")
+    .innerJoinAndSelect("stationParameter.station", "station")
+    .innerJoinAndSelect("stationParameter.parameter_type", "parameter_type")
+    .leftJoinAndSelect("stationParameter.measures", "measure")
+    .getMany();
+
+  dashBoardRepository.createQueryBuilder().select("station_parameter_id").from(StationParameter, "station_parameter").getMany();
 
     return dashBoardList;
   };
