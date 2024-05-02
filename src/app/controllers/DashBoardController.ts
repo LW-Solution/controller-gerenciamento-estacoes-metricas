992ba0fbf378a0ac4245e2e472713081af77cc19
Miguel Carvalho <doxito007@gmail.com>
2024-05-02 13:04:41 -0300

    DashBoardController.ts DashBoardRepository.ts, Fazendo Rota que retorna o Json para o DashBoard de acordo com a solicitação do PO

--- src/app/controllers/DashBoardController.ts
+++ src/app/controllers/DashBoardController.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from "express";
-import { getDahsBoardData } from "../repositories/DashBoardRepository";
+import { getDahsBoardData, getDahsBoardDataUnixTime } from "../repositories/DashBoardRepository";
 
 const dashBoardRouter = Router();
 
@@ -12,6 +12,19 @@ dashBoardRouter.get("/:id", async (req: Request, res: Response) => {
         return res.status(200).json(dashBoardData);
     } catch (error) {
         return res.status(500).json({ message: "Erro ao obter dados do dashboard", error: error.message });
+    }
+});
+
+dashBoardRouter.get("/unixtime/:unixtime/:id_station", async (req: Request, res: Response) => {
+    const { unixtime, id_station } = req.params;
+    let unixtimeStringToUnixtimeNumber = parseInt(unixtime);
+    let idStringToIdNumber = parseInt(id_station);
+    
+    try {
+        const dashBoardData = await getDahsBoardDataUnixTime(unixtimeStringToUnixtimeNumber, idStringToIdNumber);
+        return res.status(200).json(dashBoardData);
+    } catch (error) {
+        return res.status(500).json({ message: "Erro ao obter dados do dashboard por unixtime", error: error.message });
     }
 });
     
--- src/app/repositories/DashBoardRepository.ts
+++ src/app/repositories/DashBoardRepository.ts
@@ -1,3 +1,4 @@
+import { JsonObject } from "swagger-ui-express";
 import { AppDataSource } from "../../database/data-source";
 import Measure from "../entities/Measure";
 import StationParameter from "../entities/StationParameter";
@@ -9,21 +10,46 @@ const stationParameter = AppDataSource.getRepository(StationParameter);
 const dashBoardRepository = AppDataSource.getRepository(Measure);
 
 const getDahsBoardData = async (id: number): Promise<IStationParameter[]> => {
-    // const dashBoardList = await stationParameter.find({
-    //     relations: ["station", "parameter_type", "measures"],
-    // });
-    
-    const dashBoardList = await stationParameter
-    .createQueryBuilder("stationParameter")
-    .innerJoinAndSelect("stationParameter.station", "station")
-    .innerJoinAndSelect("stationParameter.parameter_type", "parameter_type")
-    .leftJoinAndSelect("stationParameter.measures", "measure")
-    .getMany();
+  const dashBoardList = await stationParameter.find({
+    relations: ["station", "parameter_type", "measures"],
+  });
 
-  dashBoardRepository.createQueryBuilder().select("station_parameter_id").from(StationParameter, "station_parameter").getMany();
+  return dashBoardList;
+};
 
-    return dashBoardList;
-  };
+const getDahsBoardDataUnixTime = async (unixtime: number, id_station: number): Promise<JsonObject> => {
 
-export { getDahsBoardData };
+  const date = new Date(unixtime * 1000);
+
+  const dashBoardList = await stationParameter.find({
+    relations: ["station", "parameter_type", "measures"],
+    where: {
+      station: {
+        id_station: id_station
+      },
+      measures: {
+        unixtime,
+      },
+    }
+  });
+
+  const formattedData: {[key: string]: string | null} = {
+        ano: String(date.getFullYear()),
+        mes: String(date.getMonth() + 1), // Os meses são indexados de 0 a 11
+        data: `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`,
+        hora: `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}:${String(date.getSeconds()).padStart(2, '0')}`,
+        id_estacao: id_station.toString(),
+    };
+
+  // Mapeia os parâmetros e seus valores
+  dashBoardList.forEach((item) => {
+    const paramName = item.parameter_type.description;
+    const paramValue = (item.measures.length > 0) ? `${item.measures[0].value * item.parameter_type.factor + item.parameter_type.offset}` : null;
+    formattedData[paramName] = paramValue;
+  });
+
+  return formattedData;
+};
+
+export { getDahsBoardData, getDahsBoardDataUnixTime };
 export default dashBoardRepository;
