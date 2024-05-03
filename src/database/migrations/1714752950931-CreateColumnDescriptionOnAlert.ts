667fec34d2326b42719959decc917f25e9b9747b
caio <caiovitordias1@gmail.com>
2024-05-03 13:56:55 -0300

    createColumnDescriptionMigration - correcao da migration para nova coluna description do alerta

--- src/database/migrations/1714752950931-CreateColumnDescriptionOnAlert.ts
+++ src/database/migrations/1714752950931-CreateColumnDescriptionOnAlert.ts
@@ -3,17 +3,28 @@ import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
 export class CreateColumnDescriptionOnAlert1714752950931 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.addColumn(
-            'alert',
-            new TableColumn({
-                name: 'description',
-                type: 'varchar', 
-                isNullable: true,
-            }),
-        );
+        const table = await queryRunner.getTable('alert');
+        const isDescriptionColumnExist = table.columns.some(c => c.name === 'description');
+        
+        if (!isDescriptionColumnExist) {
+            
+        
+            await queryRunner.addColumn(
+                'alert',
+                new TableColumn({
+                    name: 'description',
+                    type: 'varchar', 
+                    isNullable: true,
+                }),
+            );
+        }
+        
+
+       
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropColumn('alert', 'description');
     }
 
 }
